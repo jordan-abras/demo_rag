17a1f6c28edd85eb1f04ab74196e174d21859b13
jordan-abras <127291409+jordan-abras@users.noreply.github.com>
2025-06-11 20:43:43 +0200

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import openai
+from llama_index.llms.openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+
+st.set_page_config(page_title="This is a brand new RAG-LLM !", page_icon="🤖", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets.openai_key
+
+st.title("Wanna know what my research is about?")
+st.info("Let's see what I know", icon="📃")
+
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about my research!",
+        }
+    ]
+
+reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+docs = reader.load_data()
+Settings.llm = OpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0.2,
+        system_prompt="""You are an expert in information systems and generative AI research. 
+        Assume that all questions are related 
+        to this research. Keep 
+        your answers scientific and based on 
+        facts – do not hallucinate features.""",
+    )
+index = VectorStoreIndex.from_documents(docs)
+
+if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="condense_question", verbose=True, streaming=True
+    )
+
+if prompt := st.chat_input(
+    "Ask a question"
+):  # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:  # Write message history to UI
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response_stream = st.session_state.chat_engine.stream_chat(prompt)
+        st.write_stream(response_stream.response_gen)
+        message = {"role": "assistant", "content": response_stream.response}
+        # Add response to message history
+        st.session_state.messages.append(message)
